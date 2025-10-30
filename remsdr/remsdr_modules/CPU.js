361e80b67329e1ea384e606e6bdb3cb46e899f1b
F5OEO <evaristec@gmail.com>
2025-10-30 22:50:13 +0100

    Plutp is not always usb

--- remsdr/remsdr_modules/CPU.js
+++ remsdr/remsdr_modules/CPU.js
@@ -87,7 +87,7 @@ var SDRrx = "";
 var SDRtx = "";
 var hackrf_info = "";
 function Def_SDR() {
-    var info_usb = cp.execSync("lsusb").toString();
+    
     var SDRs = [];
     try {
         SDRrx = fs.readFileSync('/remsdr/data/SDRrx.txt', 'utf8');
@@ -95,44 +95,13 @@ function Def_SDR() {
     try {
         SDRtx = fs.readFileSync('/remsdr/data/SDRtx.txt', 'utf8');
     } catch (e) {}
-    if (info_usb.indexOf("ID 1df7:") > 0) {
-        SDRs.push("sdrplay|SDR Play / RSP1..");
-        if (SDRrx.length < 2)
-            SDRrx = "sdrplay";
-    }
-    if (info_usb.indexOf("RTL2838") > 0) {
-        SDRs.push("rtlsdr|RTL-SDR");
-        if (SDRrx.length < 2)
-            SDRrx = "rtlsdr";
-    }
-    if (info_usb.indexOf("HackRF One") > 0) {
-        try {
-            hackrf_info = cp.execSync("hackrf_info ").toString(); //Cannot be read if Hack RF already busy
-        } catch (e) {
-            //Use old one
-
-        }
-        var hacks = hackrf_info.split("\n");
-        var sn = "";
-        for (var i = 0; i < hacks.length; i++) {
-            if (hacks[i].indexOf("Serial number") >= 0) {
-                sn = hacks[i].substr(-8);
-                SDRs.push("hackrf" + sn + "|HackRF One:" + sn);
-                if (SDRrx.length < 2)
-                    SDRrx = "hackrf" + sn; //By default
-                if (SDRtx.length < 2)
-                    SDRtx = "hackrf" + sn;
-            }
-        }
-
-    }
-    if (info_usb.indexOf("ADALM-PLUTO") > 0) {
-        SDRs.push("pluto|Adalm Pluto");
-        if (SDRrx.length < 2)
-            SDRrx = "pluto";
-        if (SDRtx.length < 2)
-            SDRtx = "pluto";
-    }
+    
+    SDRs.push("pluto|Adalm Pluto");
+    if (SDRrx.length < 2)
+       SDRrx = "pluto";
+    if (SDRtx.length < 2)
+        SDRtx = "pluto";
+    
     SetSDR("RX", SDRrx);
     SetSDR("TX", SDRtx);
     return SDRs;
